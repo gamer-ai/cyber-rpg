cf94cac3f2300d2b2c19eea1eb350271b8773d4c
lele94218 <terry.trxue@gmail.com>
2020-10-08 00:02:10 -0700

    init commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigid2D_ = GetComponent<Rigidbody2D>();
+        animator_ = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change_ = Vector3.zero;
+        change_.x = Input.GetAxisRaw("Horizontal");
+        change_.y = Input.GetAxisRaw("Vertical");
+        MoveCharacter();
+        AnimateCharacter();
+    }
+
+    void MoveCharacter()
+    {
+        Vector3 to = transform.position + change_ * speed_ *
+            Time.deltaTime;
+        playerRigid2D_.MovePosition(to);
+    }
+
+    void AnimateCharacter()
+    {
+        if (change_ != Vector3.zero)
+        {
+            animator_.SetBool("isMoving", true);
+            animator_.SetFloat("moveX", change_.x);
+            animator_.SetFloat("moveY", change_.y);
+        }
+        else
+        {
+            animator_.SetBool("isMoving", false);
+        }
+    }
+
+    public float speed_;
+    private Rigidbody2D playerRigid2D_;
+    private Vector3 change_;
+    private Animator animator_;
+}
