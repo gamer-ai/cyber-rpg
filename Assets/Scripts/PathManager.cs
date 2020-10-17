e85566401bcb06c26efa463c2a31cb5df91adfdc
lele94218 <terry.trxue@gmail.com>
2020-10-17 01:37:01 -0700

    Add a star path finding class and world to manage tilemaps

---
+++ Assets/Scripts/PathManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathManager : MonoBehaviour
+{
+    public static PathManager Instance
+    {
+        get
+        {
+            if (instance_ == null)
+            {
+                instance_ = FindObjectOfType<PathManager>();
+            }
+            return instance_;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        List<PathFind.Point> points =
+            PathFind.Pathfinding.FindPath(world.CurrentGrid.Value,
+            new PathFind.Point(0, 0), new PathFind.Point(10, 10));
+        foreach (var point in points)
+        {
+            Debug.LogFormat("{0}, {1}", point.x, point.y);
+        }
+    }
+
+    public List<Vector2> FindCurrentPath(Vector2 worldStart, Vector2 wordTarget)
+    {
+        return null;
+    }
+
+    public World world;
+    private static PathManager instance_;
+}
---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class World : MonoBehaviour
+{
+    public const int MAX_WIDTH_OF_ROOM = 100;
+    public const int MAX_HEIGHT_OF_ROOM = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Tilemaps = GetComponentsInChildren<Tilemap>();
+        Grid[] grids = GetComponentsInChildren<Grid>();
+        GridsDict = new Dictionary<string, Grid>();
+        foreach (Grid grid in grids)
+        {
+            GridsDict.Add(grid.name, grid);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Tilemap tilemap in Tilemaps)
+        {
+            if (tilemap.name == "Ground")
+            {
+                Vector3 worldCenter =
+                    tilemap.CellToWorld(
+                        new Vector3Int(
+                            Mathf.RoundToInt(tilemap.cellBounds.center.x),
+                            Mathf.RoundToInt(tilemap.cellBounds.center.y), 0));
+                Bounds bounds = new Bounds(worldCenter,
+                                    tilemap.cellBounds.size);
+                Bounds worldBounds = bounds;
+                if (worldBounds.Contains(target.position))
+                {
+
+                    string roomName = tilemap.transform.parent.name;
+                    if (CurrentGrid.Key != roomName)
+                    {
+                        if (!GridsDict.ContainsKey(roomName))
+                        {
+                            Debug.LogErrorFormat(
+                                "Didn't find room with name: {0}", roomName);
+                        }
+                        else
+                        {
+                            CurrentGrid =
+                                new KeyValuePair<string, PathFind.Grid>(
+                                    roomName, BuildGrid(GridsDict[roomName]));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private PathFind.Grid BuildGrid(Grid grid)
+    {
+        Tilemap ground = grid.transform.Find("Ground").GetComponent<Tilemap>();
+        Tilemap collision =
+            grid.transform.Find("Collision").GetComponent<Tilemap>();
+        int width = ground.cellBounds.size.x;
+        int height = ground.cellBounds.size.y;
+        bool[,] movableTiles = new bool[MAX_WIDTH_OF_ROOM, MAX_HEIGHT_OF_ROOM];
+        for (int x = 0; x < width; ++x)
+        {
+            for (int y = 0; y < height; ++y)
+            {
+                // All cell should be unreachable by default.
+                movableTiles[x, y] = false;
+                if (ground.HasTile(new Vector3Int(x, y, 0)))
+                {
+                    movableTiles[x, y] = true;
+                }
+                if (collision.HasTile(new Vector3Int(x, y, 0)))
+                {
+                    movableTiles[x, y] = false;
+                }
+            }
+        }
+        PathFind.Grid pathGrid = new PathFind.Grid(width, height, movableTiles);
+        return pathGrid;
+    }
+
+    public Tilemap[] Tilemaps { get; private set; }
+    public Dictionary<string, Grid> GridsDict { get; private set; }
+    public KeyValuePair<string, PathFind.Grid> CurrentGrid { get; private set; }
+    public Transform target;
+}
