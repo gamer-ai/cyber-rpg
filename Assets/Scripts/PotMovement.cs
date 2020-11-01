39e52b2c19a88c88ff41dd91f2850ca6ce7ace4b
lele94218 <terry.trxue@gmail.com>
2020-11-01 15:56:26 -0800

    add hitbox

---
+++ Assets/Scripts/PotMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PotMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator_ = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Smash()
+    {
+        animator_.SetBool("isSmash", true);
+        StartCoroutine(ExitWorld());
+    }
+
+    private IEnumerator ExitWorld()
+    {
+        yield return new WaitForSeconds(0.3f);
+        gameObject.SetActive(false);
+    }
+
+    private Animator animator_;
+}
